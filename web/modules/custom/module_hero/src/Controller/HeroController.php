4c0ac20659fca26886b329b502f552d783171121
Rajesh Yadav <yadavrajesh25june@gmail.com>
2020-05-06 12:51:39 +0530

    add custom template

--- web/modules/custom/module_hero/src/Controller/HeroController.php
+++ web/modules/custom/module_hero/src/Controller/HeroController.php
@@ -14,7 +14,7 @@ class HeroController extends ControllerBase {
 
   public function heroList() {
 
-    $heros = [
+    $heroes = [
       ['name'=>'Hulk'],
       ['name'=>'Thor'],
       ['name'=>'Iron Man'],
@@ -23,6 +23,7 @@ class HeroController extends ControllerBase {
 
     ];
 
+    /*
     $ourHeros = '';
     foreach ($heros as $hero) {
       $ourHeros .= '<li>'. $hero['name']. '</li>';
@@ -31,6 +32,13 @@ class HeroController extends ControllerBase {
     return [
       '#type' => 'markup',
       '#markup' => '<h4>'.$this->t('These are our best heros'). '</h4><ol>'.$ourHeros.'</ol>',
+    ];
+    */
+
+    return [
+      '#theme' => 'hero_list',
+      '#items'  =>  $heroes,
+      '#title' => $this->t('Our wonderful heroes list'),
     ];
 
   }
