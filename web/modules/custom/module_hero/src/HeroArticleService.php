6fa1aff330230c37865c7fc70f8cc68d0c13e8fc
Rajesh Yadav <yadavrajesh25june@gmail.com>
2020-05-07 08:20:23 +0530

    Create sample custom services

---
+++ web/modules/custom/module_hero/src/HeroArticleService.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Drupal\module_hero;
+
+/**
+* Our heros article service class.
+*/
+class HeroArticleService {
+
+  /**
+  * Our heros article service class.
+  */
+  public function getHeroArticles() {
+
+    $articles = ['Sky is blue','Nature is Green'];
+
+    return $articles;
+  }
+}
